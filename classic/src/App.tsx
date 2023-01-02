e1f37423f55e48676aa40fe924524b25c75315cc
nathgilson <nth.glsn@gmail.com>
2023-01-02 19:52:47 +0100

    cleaned css

--- classic/src/App.tsx
+++ classic/src/App.tsx
@@ -6,6 +6,7 @@ import Error from './components/Error'
 import PaymentPage1 from './components/PaymentPage1'
 import PaymentPage2 from './components/PaymentPage2'
 import PaymentPage from './components/PaymentPage'
+import PaymentPage3 from './components/PaymentPage3'
 
 export default (): React.ReactElement => {
   const { loading } = useAuth()
@@ -13,7 +14,7 @@ export default (): React.ReactElement => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<PaymentPage />} />
+        <Route path='/' element={<PaymentPage3 />} />
         <Route path='/pay' element={<PaymentPage1 />} />
         <Route path='/pay2' element={<PaymentPage2 />} />
         <Route path='/paymentSuccessful' element={<Success />} />
